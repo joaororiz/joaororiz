f3dcf707821f28decf27e47584de5160a49bd993
João Otávio Mota Roriz <joao.otvo.rv@gmail.com>
2019-05-27 18:44:14 -0300

    Finalizando

---
+++ 9º Periodo/Manuteção e Evolução de software/AplicaçãoAgConsultas/src/Conexao/Conetion.java
@@ -0,0 +1,22 @@
+package Conexao;
+
+//package Fabrica;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+///**
+// *
+// * @author joaoo
+// */
+//public class Conetion {
+//
+//    public Connection getConnection() {
+//        try {
+//            return DriverManager.getConnection("jdbc:mysql://localhost/consultasbd", "root", "");
+//        } catch (SQLException excecao) {
+//            throw new RuntimeException(excecao);
+//        }
+//    }
+//}
